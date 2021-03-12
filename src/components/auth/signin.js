4ff77f60c0fd79de7e9bf521afadc78bb229e2ab
casline <c_schafrath@yahoo.com>
2021-03-11 20:27:52 -0500

    created a pageTitle component and started to style using google fonts

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import SigninForm from "./signinForm";
 
+import PageTitle from "../pageTitle";
+
 class Signin extends Component {
   onSubmit = (fields) => {
     console.log(fields);
@@ -8,6 +10,7 @@ class Signin extends Component {
   render() {
     return (
       <div className="sign-in">
+        <PageTitle className="sign-in__page-title" title="Login" />
         <SigninForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     );
