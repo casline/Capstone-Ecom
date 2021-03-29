cb87bf17638e26b624fb22cd55b23e232f873bcb
casline <c_schafrath@yahoo.com>
2021-03-29 13:38:45 -0400

    updated user info for user.js(actions) also updated purchaseHistory.js and finished purchases.js component

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,13 +6,91 @@ export function fetchUserPurchases() {
     payload: [
       {
         _id: 0,
-        title: "Coding in Progress",
-        amount: 8.02,
+        total: 4.76,
+        orderNumber: "A00465793",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Rhett Schafarath",
+          shippingAddress: "1234 North Point Street",
+        },
       },
       {
         _id: 1,
-        title: "Purchase 1",
-        amount: 4.67,
+        total: 14.46,
+        orderNumber: "A00976473",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Veda Schafarath",
+          shippingAddress: "1234 South Point Street",
+        },
+      },
+      {
+        _id: 2,
+        total: 6.76,
+        orderNumber: "A98647326",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Caroline Schafarath",
+          shippingAddress: "1234 East Point Street",
+        },
+      },
+      {
+        _id: 3,
+        total: 15.76,
+        orderNumber: "A95362716",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Caleb Schafarath",
+          shippingAddress: "1234 West Point Street",
+        },
+      },
+      {
+        _id: 4,
+        total: 24.16,
+        orderNumber: "A54672916",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Cy Schafarath",
+          shippingAddress: "1234 Columbus Street",
+        },
+      },
+      {
+        _id: 5,
+        total: 13.56,
+        orderNumber: "A32785395",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Clete Schafarath",
+          shippingAddress: "1234 North Atlanta Street",
+        },
+      },
+      {
+        _id: 6,
+        total: 1.76,
+        orderNumber: "A37285640",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Carmi Schafarath",
+          shippingAddress: "1234 North South Street",
+        },
+      },
+      {
+        _id: 7,
+        total: 35.46,
+        orderNumber: "A12647839",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Mike Schafarath",
+          shippingAddress: "1234 North High Street",
+        },
       },
     ],
   };
