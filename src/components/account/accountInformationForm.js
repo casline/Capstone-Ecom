81410ce927db764e14a3edd925d897e78b4c4a1d
casline <c_schafrath@yahoo.com>
2021-04-05 13:16:23 -0400

    worked on accountInformationForm.js and formFields.js in implementing change password functions

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import history from "../../history";
 
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton, LongGrayButton } from "../formFields";
 
 class AccountInformationForm extends Component {
   render() {
@@ -70,15 +70,18 @@ class AccountInformationForm extends Component {
           name="password"
           component={FormInput}
         />
-        <div className="account-information-form__line"></div>
+        <div className="account-information-form__line"></div> */}
         <Field
-          className="account-information-form__login"
-          onClick={() => history.push("/account")}
-          type="submit"
-          title="Login"
-          name="login"
-          component={FormButton}
-        /> */}
+          className="account-information-form__change-password"
+          onClick={() =>
+            console.log("coding in progress, tryna show passwords")
+          }
+          type="button"
+          labelTitle="Password"
+          title="Change Password"
+          name="change-password"
+          component={LongGrayButton}
+        />
       </form>
     );
   }
