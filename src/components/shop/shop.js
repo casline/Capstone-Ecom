5678d3fa14351b252c12c81621147b7f8b92085a
casline <c_schafrath@yahoo.com>
2021-04-18 16:45:51 -0400

    previously added shopCart component and stylesheet for it , then currently further built out shopCart component and styled it

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
-import { Field } from "redux-form";
 import * as actions from "../../actions";
-import ShopProduct from "./shopProduct";
 import ShopSearchBar from "./shopSearchBar";
+import ShopProduct from "./shopProduct";
+import ShopCart from "./shopCart";
 
 class Shop extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showCart: true,
+    };
+  }
+
   componentDidMount() {
     const headerLinks = [
       {
         _id: 0,
         title: "Login",
-        path: "./signin",
+        path: "/signin",
       },
     ];
     this.props.setHeaderLinks(headerLinks);
     this.props.fetchShopCategories();
+
     // filter products with links
     this.props.fetchShopProducts();
   }
@@ -35,15 +44,18 @@ class Shop extends Component {
   };
 
   render() {
+    return <ShopCart className="shop__cart" />;
+
     return (
       <div className="shop">
-        Coding in progress
         <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
         <div className="shop__products">
           {this.props.filteredProducts.map((product) => {
             return <ShopProduct {...product} key={product._id} />;
           })}
         </div>
+        {this.state.showCart ? <ShopCart className="shop__cart" /> : ""}
+
         {/* shop cart button */}
       </div>
     );
@@ -61,3 +73,72 @@ function mapStateToProps(state) {
 Shop = connect(mapStateToProps, actions)(Shop);
 
 export default Shop;
+
+// import React, { Component } from "react";
+
+// import { connect } from "react-redux";
+// import { Field } from "redux-form";
+// import * as actions from "../../actions";
+// import ShopProduct from "./shopProduct";
+// import ShopSearchBar from "./shopSearchBar";
+// import ShopCart from "./shopCart";
+
+// class Shop extends Component {
+//   componentDidMount() {
+//     const headerLinks = [
+//       {
+//         _id: 0,
+//         title: "Login",
+//         path: "./signin",
+//       },
+//     ];
+//     this.props.setHeaderLinks(headerLinks);
+//     this.props.fetchShopCategories();
+//     // filter products with links
+//     this.props.fetchShopProducts();
+//   }
+
+//   shouldComponentUpdate(nextProps) {
+//     if (this.props != nextProps) {
+//       this.props.setNavbarLinks(nextProps.categories, (_id) =>
+//         this.props.filterProductsWithCategoryId(_id)
+//       );
+//     }
+//     return true;
+//   }
+
+//   onSubmit = (fields) => {
+//     this.props.filterProductsWithQuery(fields);
+//   };
+
+//   render() {
+//     return (
+//       <div className="shop">
+//         Coding in progress
+//         <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
+//         <div className="shop__products">
+//           {this.props.filteredProducts.map((product) => {
+//             return <ShopProduct {...product} key={product._id} />;
+//           })}
+//         </div>
+//         {
+//                   {this.state.showCart ? <ShopCart className="shop__cart" /> : ""}
+
+//         }
+//         {/* shop cart button */}
+//       </div>
+//     );
+//   }
+// }
+
+// function mapStateToProps(state) {
+//   const { categories, filteredProducts } = state.shop;
+//   return {
+//     categories,
+//     filteredProducts,
+//   };
+// }
+
+// Shop = connect(mapStateToProps, actions)(Shop);
+
+// export default Shop;
