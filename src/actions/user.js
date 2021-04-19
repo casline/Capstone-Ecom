397a688ddf53633fece216762c04cc71f43d9d06
casline <c_schafrath@yahoo.com>
2021-04-19 18:35:39 -0400

    further worked on CartProduct and setup action creator to fetch and set user cart products

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,9 +1,45 @@
-import { SET_USER_PURCHASES, SET_PURCHASE_DETAIL } from "./types";
+import {
+  SET_USER_PURCHASES,
+  SET_PURCHASE_DETAIL,
+  SET_CART_PRODUCTS,
+} from "./types";
 
 export function setPurchaseDetail(_id) {
   return {
     type: SET_PURCHASE_DETAIL,
     payload: _id,
+  };
+}
+
+export function fetchCartProducts() {
+  return {
+    type: SET_CART_PRODUCTS,
+    payload: [
+      {
+        _id: 0,
+        product: {
+          _id: 0,
+          title: "Multi-Colored Decals",
+          description:
+            "With these sets , you can choose a multi-colored design and show off decals with a multitude of colors and design options.",
+          price: 2.99,
+          belongsTo: [0, 1],
+        },
+        quantity: 2,
+      },
+      {
+        _id: 1,
+        product: {
+          _id: 1,
+          title: "Black Decals",
+          description:
+            "With these decals , you can choose a multitude of black only decals that will give your design a destictive pop. Also comes in matte finish.",
+          price: 1.99,
+          belongsTo: [0, 2],
+        },
+        quantity: 1,
+      },
+    ],
   };
 }
 
