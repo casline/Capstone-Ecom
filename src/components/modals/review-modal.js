5b8a3fd493732f58088466ab12dc323dbbcfc72a
casline <c_schafrath@yahoo.com>
2021-05-03 12:26:57 -0400

    fixed an error message dealing with the modal

--- src/components/modals/review-modal.js
+++ src/components/modals/review-modal.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
 
+ReactModal.setAppElement(".app-wrapper");
+
 class ReviewModal extends Component {
   constructor(props) {
     super(props);
